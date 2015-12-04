832b09fac48fb177533a47e0cb160b170ae8f0fd
root <root@panyao.(none)>
2015-12-05 07:26:16 +0800

    add build file

---
+++ run/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+MYBIN=$PWD/bin
+mkdir -p $MYBIN
+
+# Build aircrack
+pushd ../aircrack-ng-1.2-rc3/
+make clean
+make
+cp scripts/airmon-ng $MYBIN/
+cp src/airodump-ng $MYBIN/
+popd
+
+# Build reaver
+pushd ../reaver-1.4/src
+./configure
+make clean
+make
+cp reaver $MYBIN/
+popd
+
